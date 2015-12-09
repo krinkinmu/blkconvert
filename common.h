8bb5b356fcd5c9f962c68314e7e3331f447277f3
Mike Krinkin <krinkin.m.u@gmail.com>
2015-12-09 18:55:17 +0300

    rewrite common macros

--- common.h
+++ common.h
@@ -2,9 +2,27 @@
 #define __COMMON_H__
 
 #include <stddef.h>
+#include <stdint.h>
 
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN_CONST(a, b) ((a) < (b) ? (a) : (b))
+#define MAX_CONST(a, b) ((a) > (b) ? (a) : (b))
+
+static inline intmax_t __min(intmax_t l, intmax_t r)
+{ return MIN_CONST(l, r); }
+
+static inline uintmax_t __minu(uintmax_t l, uintmax_t r)
+{ return MIN_CONST(l, r); }
+
+static inline intmax_t __max(intmax_t l, intmax_t r)
+{ return MAX_CONST(l, r); }
+
+static inline uintmax_t __maxu(uintmax_t l, uintmax_t r)
+{ return MAX_CONST(l, r); }
+
+#define MIN(a, b)  __min(a, b)
+#define MINU(a, b) __minu(a, b)
+#define MAX(a, b)  __max(a, b)
+#define MAXU(a, b) __maxu(a, b)
 
 #define container_of(ptr, type, member) \
 	((type *)(((char *)(ptr)) - offsetof(type, member)))
