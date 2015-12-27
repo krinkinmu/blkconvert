b325674cb1c6519c16bad2feb95cd65861d1207a
Mike Krinkin <krinkin.m.u@gmail.com>
2015-12-27 19:11:43 +0300

    common demon creation routine

---
+++ deamon.c
@@ -0,0 +1,41 @@
+#include "deamon.h"
+#include "debug.h"
+
+#include <unistd.h>
+
+#include <stdlib.h>
+#include <errno.h>
+
+int deamon(const char *name, deamon_fptr_t fptr)
+{
+	const pid_t pid = fork();
+
+	if (pid < 0)
+		return errno;
+
+	if (pid == 0) {
+		fclose(stdin);
+		fclose(stdout);
+		openlog(name, 0, 0);
+		redirect_to_syslog(&stderr);
+
+		const pid_t sid = setsid();
+
+		if (sid < 0) {
+			ERR("Cannot create a new session (%d)\n", errno);
+			exit(1);
+		}
+
+		if (chdir("/")) {
+			ERR("Cannot change dir to \"/\" (%d)\n", errno);
+			exit(1);
+		}
+		fptr();
+		closelog();
+		exit(0);
+	} else {
+		exit(0);
+	}
+
+	return 0;
+}
