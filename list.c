8f7670dbd76696ee2c3dd6f6efb8bb3b3af43a88
Mike Krinkin <krinkin.m.u@gmail.com>
2016-01-11 14:33:21 +0300

    reimplement split and add split_tail

--- list.c
+++ list.c
@@ -21,19 +21,23 @@ void list_unlink(struct list_head *pos)
 	pos->next->prev = pos->prev;
 }
 
-static void __list_splice(struct list_head *pos, struct list_head *head,
-			struct list_head *tail)
+static void __list_splice(struct list_head *prev, struct list_head *next,
+			struct list_head *head, struct list_head *tail)
 {
-	head->prev = pos;
-	pos->next = head;
-	tail->next = pos;
-	pos->prev = tail;
+	prev->next = head;
+	head->prev = prev;
+
+	tail->next = next;
+	next->prev = tail;
 }
 
 void list_splice(struct list_head *pos, struct list_head *lst)
 {
 	if (list_empty(lst))
 		return;
-	__list_splice(pos, lst->next, lst->prev);
+	__list_splice(pos, pos->next, lst->next, lst->prev);
 	list_head_init(lst);
 }
+
+void list_splice_tail(struct list_head *pos, struct list_head *lst)
+{ list_splice(pos->prev, lst); }
