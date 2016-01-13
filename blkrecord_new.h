181ab0aa4f8d1ecc104aa03c1dd3a149258221cd
Mike Krinkin <krinkin.m.u@gmail.com>
2016-01-13 16:37:04 +0300

    define network protocol

--- blkrecord_new.h
+++ blkrecord_new.h
@@ -58,6 +58,7 @@ struct blkio_record_conf {
 	const char *device;
 	size_t buffer_size;
 	size_t buffer_count;
+	size_t events_count;
 	int poll_timeout;
 };
 
@@ -69,5 +70,63 @@ struct blkio_record_ctx {
 	int cpus;
 	int fd;
 };
+
+int blkio_record_ctx_setup(struct blkio_record_ctx *ctx,
+			struct blkio_record_conf *conf);
+int blkio_trace_start(struct blkio_record_ctx *ctx);
+void blkio_trace_stop(struct blkio_record_ctx *ctx);
+int blkio_trace_drops(struct blkio_record_ctx *ctx);
+void blkio_record_ctx_release(struct blkio_record_ctx *ctx);
+
+#include <stdint.h>
+
+#define BLKIO_MSG_START  1
+#define BLKIO_MSG_STOP   2
+#define BLKIO_MSG_STATS  3
+#define BLKIO_MSG_STATUS 4
+
+#define BLKIO_MAX_PATH   256
+
+#define BLKIO_STATUS_OK     0
+#define BLKIO_STATUS_ERROR  1
+
+struct blkio_net_hdr {
+	uint32_t type;
+	uint32_t size;
+} __attribute__((packed));
+
+struct blkio_net_start {
+	struct blkio_net_hdr hdr;
+	char device[BLKIO_MAX_PATH];
+	uint32_t buffer_size;
+	uint32_t buffer_count;
+	uint32_t events_count;
+	uint32_t poll_timeout;
+} __attribute__((packed));
+
+struct blkio_net_stop {
+	struct blkio_net_hdr hdr;
+} __attribute__((packed));
+
+struct blkio_net_stats {
+	struct blkio_net_hdr hdr;
+	struct blkio_stats stats;
+} __attribute__((packed));
+
+struct blkio_net_status {
+	struct blkio_net_hdr hdr;
+	uint32_t error;
+	uint32_t drops;
+} __attribute__((packed));
+
+union blkio_net_storage {
+	struct blkio_net_hdr hdr;
+	struct blkio_net_start start;
+	struct blkio_net_stop stop;
+	struct blkio_net_stats stats;
+	struct blkio_net_status status;
+};
+
+int blkio_net_read(int fd, void *buffer);
 
 #endif /*__BLKRECORD_H__*/
